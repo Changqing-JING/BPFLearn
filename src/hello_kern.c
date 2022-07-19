3a7d6a4de80568c98ca2a0d1fbf97d1786e60a91
Changqing Jing <changqing.jing@bmw.com>
2022-07-19 17:14:01 +0800

    add map example

--- src/hello_kern.c
+++ src/hello_kern.c
@@ -1,18 +1,32 @@
-
-//#include <uapi/linux/bpf.h>
 #include <linux/types.h>
+
 #include <bpf/bpf_helpers.h>
 
+#include <linux/bpf.h>
+
+struct {
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __type(key, __u32);         /* class; u32 required */
+  __type(value, __u32);       /* count of mads read */
+  __uint(max_entries, 1024U); /* Room for all Classes */
+} my_map SEC(".maps");
 
 SEC("kprobe/__seccomp_filter")
-int bpf_prog1(struct pt_regs *ctx)
-{
-    const char msg[] = {'h', 'e', 'l', 'l', 'o', ' ', 'b', 'p', 'f', '\n', 0};
- 
-    bpf_trace_printk(msg, sizeof(msg));
 
-	return 0;
+int bpf_prog1(struct pt_regs *ctx) {
+  __u32 const key = 0;
+
+  __u32 *val = bpf_map_lookup_elem(&my_map, &key);
+  if (val != NULL) {
+    *(val) += 1U;
+    bpf_map_update_elem(&my_map, &key, val, BPF_ANY);
+  }
+
+  const char msg[] = {'h', 'e', 'l', 'l', 'o', ' ', 'b', 'p', 'f', '\n', 0};
+
+  bpf_trace_printk(msg, sizeof(msg));
+
+  return 0;
 }
 
 char _license[] SEC("license") = "GPL";
-
--- src/hello_user.cpp
+++ src/hello_user.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
+#include <fcntl.h>
 #include <linux/filter.h>
 #include <linux/seccomp.h>
 #include <stdexcept>
@@ -9,8 +10,6 @@
 #include <sys/prctl.h>
 #include <sys/resource.h>
 #include <unistd.h>
-
-#include "helper/trace_helpers.hpp"
 
 int main(int ac, char **argv) {
   struct bpf_link *link = NULL;
@@ -37,6 +36,25 @@ int main(int ac, char **argv) {
       throw std::runtime_error("bpf_object__load failed");
     }
 
+    const char *mapName = "my_map";
+
+    struct bpf_map *map = bpf_object__find_map_by_name(obj, mapName);
+
+    if (libbpf_get_error(map)) {
+      throw std::runtime_error("bpf_object__find_map_by_name failed");
+    }
+
+    uint32_t const mapSize = bpf_map__max_entries(map);
+
+    if (mapSize == 0) {
+      throw std::runtime_error("wrong mapSize");
+    }
+
+    int32_t const mapFd = bpf_object__find_map_fd_by_name(obj, mapName);
+
+    uint32_t key = 0;
+    uint32_t value = 0;
+
     link = bpf_program__attach(prog);
     if (libbpf_get_error(link)) {
       fprintf(stderr, "ERROR: bpf_program__attach failed\n");
@@ -44,7 +62,39 @@ int main(int ac, char **argv) {
       throw std::runtime_error("bpf_program__attach failed");
     }
 
-    read_trace_pipe();
+    int32_t const trace_fd =
+        open("/sys/kernel/debug/tracing/trace_pipe", O_RDONLY, 0);
+    if (trace_fd < 0) {
+      throw std::runtime_error("open trace_pipe failed\n");
+    }
+
+    int32_t error;
+
+    error = bpf_map_update_elem(mapFd, &key, &value, BPF_ANY);
+
+    if (error != 0) {
+      throw std::runtime_error("bpf_map_update_elem failed");
+    }
+
+    while (true) {
+
+      static char buf[4096];
+
+      ssize_t const sz = read(trace_fd, buf, sizeof(buf) - 1);
+      if (sz > 0) {
+        buf[sz] = 0;
+        puts(buf);
+      }
+
+      error = bpf_map_lookup_elem(mapFd, &key, &value);
+
+      if (error != 0) {
+        throw std::runtime_error("bpf_map_lookup_elem failed");
+      }
+
+      printf("bpf_map_lookup_elem get value %d\n", value);
+    }
+
   }
 
   catch (...) {
